500506f3c4ee00ad20ee4eac24938dd81d96f774
Oula Kuuva <oula.kuuva@iki.fi>
2025-12-24 14:59:25 +0200

    fix: use scratch-buffer in View.show
    It's used to show static text that's anyway discarded after closing.
    Using a scratch-buffer sets the `buftype` to `nofile` automatically, and
    seems to automatically prevent LSPs from attaching to the buffer. Though
    YMMV.

--- lua/codesettings/commands/view.lua
+++ lua/codesettings/commands/view.lua
@@ -3,7 +3,7 @@ local M = {}
 ---Show given string in a centered floating window, with q and esc bound to close
 ---@param str string|string[]
 function M.show(str)
-  local buf = vim.api.nvim_create_buf(false, false)
+  local buf = vim.api.nvim_create_buf(false, true)
   local vpad = 6
   local hpad = 20
 
@@ -31,7 +31,6 @@ function M.show(str)
 
   local buf_scope = { buf = buf }
   vim.api.nvim_set_option_value('filetype', 'markdown', buf_scope)
-  vim.api.nvim_set_option_value('buftype', 'nofile', buf_scope)
   vim.api.nvim_set_option_value('bufhidden', 'wipe', buf_scope)
   vim.api.nvim_set_option_value('modifiable', false, buf_scope)
 
