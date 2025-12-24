af4cb300f1dd0edcf62fd1cb131cfb1b393a9260
Oula Kuuva <oula.kuuva@iki.fi>
2025-12-24 14:59:25 +0200

    fix: set buffer options before opening window
    I was thinking that maybe the ordering was the reason why LSP clients
    (mainly otter.nvim) was insisting on attaching to the buffer even though
    the buffer options should've prevented it in my personal config. It
    ended up not being the case, but it still might be a good idea in case
    it affects something else down the line.

--- lua/codesettings/commands/view.lua
+++ lua/codesettings/commands/view.lua
@@ -29,13 +29,13 @@ function M.show(str)
   opts.row = (vim.o.lines - opts.height) / 2
   opts.col = (vim.o.columns - opts.width) / 2
 
-  local win = vim.api.nvim_open_win(buf, true, opts)
-
   local buf_scope = { buf = buf }
   vim.api.nvim_set_option_value('filetype', 'markdown', buf_scope)
   vim.api.nvim_set_option_value('buftype', 'nofile', buf_scope)
   vim.api.nvim_set_option_value('bufhidden', 'wipe', buf_scope)
   vim.api.nvim_set_option_value('modifiable', false, buf_scope)
+
+  local win = vim.api.nvim_open_win(buf, true, opts)
 
   local win_scope = { win = win }
   vim.api.nvim_set_option_value('conceallevel', 3, win_scope)
